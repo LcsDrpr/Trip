1eda7b67ccb869aaff4600f32306403309917483
Lucas Drapier <lucas.drapier@gmail.com>
2019-06-12 19:01:51 +0200

    ajout mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-//Tableau des trips
-var travel = [
-  {
-    name:"Paris",
-    image:"images/photo1.jpg",
-    description:"Paris, capitale de la France, est une grande ville européenne et un centre mondial de l'art, de la mode, de la gastronomie et de la culture. Son paysage urbain du XIXe siècle est traversé par de larges boulevards et la Seine. Outre les monuments comme la tour Eiffel et la cathédrale gothique Notre-Dame du XIIe siècle, la ville est réputée pour ses cafés et ses boutiques de luxe bordant la rue du Faubourg-Saint-Honoré."
-  },
-  {
-    name:"Panama",
-    image:"images/photo2.jpg",
-    description:"Le Panama est un pays situé sur l'isthme rattachant l'Amérique centrale et l'Amérique du Sud. Le canal de Panama, célèbre prouesse d'ingénierie, coupe cet isthme en son centre pour relier les océans Atlantique et Pacifique, créant ainsi une voie de navigation essentielle. Dans la capitale du même nom, les gratte-ciel modernes, casinos et discothèques contrastent avec les bâtiments de style colonial du quartier de Casco Viejo et la forêt tropicale du parc naturel métropolitain."
-  },
-  {
-    name:"Bora-Bora",
-    image:"images/photo3.jpg",
-    description:"Bora-Bora est une petite île du Pacifique sud, située au nord-ouest de Tahiti, en Polynésie française. Entourée d'îlots de sable, appelés 'motus', et d'une eau turquoise protégée par un récif corallien, l'île est un haut lieu de la plongée sous-marine. C'est également une destination touristique prisée pour ses complexes de luxe, dont certains proposent des bungalows sur pilotis. Au centre de l'île s'élève le mont Otemanu, un volcan endormi culminant à 727 m."
+var options = {
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true
+}
+mongoose.connect('mongodb+srv://capsuletest:azerty@cluster0-iutb9.mongodb.net/tripApp?retryWrites=true&w=majority', 
+  options,     
+  function(err) {
+   console.log(err);
   }
-]
+);
+
+var travelSchema = mongoose.Schema({
+  tripName: String,
+  description: String,
+  image: String,
+});
+
+var TripModel = mongoose.model('trips', travelSchema);
+
 
 //Route par défaut.
 router.get('/', function(req, res, next) {
-  res.render('index', {travel});
+  res.render('index');
 });
 
 router.get('/trips', function(req, res, next) {
-  res.render('trips', {travel});
+
+  TripModel.find( function (err, trips) {
+
+    res.render('trips', { title: 'Trips',trips });
+
+  });
 });
 
 router.get('/card', function(req, res, next) {
 
-  var position = req.query.position;
-  var travelName = travel[position].name;
-  var travelImg = travel[position].image;
-  var travelDesc = travel[position].description;
+  TripModel.findOne( {_id:req.query.id} ,function (err, trips) {
 
-  res.render('card', {travel,travelName,travelImg,travelDesc,position});
+    res.render('card', { title: 'Card',trips });
+
+  });
+
 });
 
 var deletedTrip = [];
@@ -55,6 +62,27 @@ router.post('/delete-post', function(req, res, next) {
   res.render('trips', {travel});
 });
 
+router.post('/new-trip', function(req, res, next) {
+  
+
+  var newTrip = new TripModel ({
+    tripName: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+  });
+
+  newTrip.save(
+    function (error, trips) {
+
+      //CityModel.find( function (err, cities) {
+    
+        res.render('index', { title: 'Trips',trips });
+    
+      //});
+    }
+  );
+});
+
 
 
 /*router.post('/delete-post', function(req, res, next) {
@@ -71,7 +99,7 @@ router.post('/delete-post', function(req, res, next) {
 router.get('/new', function(req, res, next) {
 
   //console.log(deletedTrip);
-  res.render('new', {travel,deletedTrip});
+  res.render('new', {deletedTrip});
 });
 
 
